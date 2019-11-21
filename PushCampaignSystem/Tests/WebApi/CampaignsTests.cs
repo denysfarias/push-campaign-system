bac84fde20395d46953f213cb4ce85bdc60e9365
Denys <denys.lf@gmail.com>
2019-11-21 17:28:32 -0300

    campaigns endpoint get test

---
+++ PushCampaignSystem/Tests/WebApi/CampaignsTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class CampaignsTests
+    {
+        private void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetTest()
+        {
+            Setup();
+
+            throw new NotImplementedException();
+        }
+    }
+}
