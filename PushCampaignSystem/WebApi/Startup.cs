fb2c2dc29de7811a1210a90a2440478edbf5e655
Denys Farias <denys.lf@gmail.com>
2019-11-24 01:07:01 -0300

    Fixed dependecy injection;

--- PushCampaignSystem/WebApi/Startup.cs
+++ PushCampaignSystem/WebApi/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -11,6 +12,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using WebApi.Controllers;
+using WebApi.Models;
+using WebApi.PushCampaignService;
+using WebApi.PushCampaignService.DataStore;
+using WebApi.PushCampaignService.Domain;
+using WebApi.PushCampaignService.Domain.DataStore;
+using WebApi.PushCampaignService.Domain.PushNotificationProvider;
+using WebApi.PushNotificationProviders;
 
 namespace WebApi
 {
@@ -33,6 +42,18 @@ namespace WebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Push Campaign API", Version = "v1" });
             });
+
+            // Dependecy Injection
+            services.AddSingleton(typeof(ILogger<CampaignsController>), typeof(Logger<CampaignsController>));
+            services.AddSingleton(typeof(ILogger<VisitsController>), typeof(Logger<VisitsController>));
+
+            services.AddTransient<ICampaignManager, CampaignManager>();
+            services.AddTransient<IVisitManager, VisitManager>();
+            services.AddTransient<IDataStore<Campaign>, MockCampaignStore>();
+            services.AddTransient<IDataStore<Visit>, MockVisitStore>();
+            services.AddTransient<ICampaignSearch, SimpleCampaignSearch>();
+            services.AddTransient<IPushNotificationProviderFactory, PushNotificationProviderFactory>();
+            services.AddTransient<TextWriter>(service => Console.Out);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
